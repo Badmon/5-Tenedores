e7869c913c40dfef4fdf822700534355edc824b1
Juan Antonio <jantoleonh@gmail.com>
2021-01-07 02:54:58 -0500

    Funcion para validar emails

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navigation from "./app/navigations/Navigation"
-import { firebaseApp } from "./app/Utils/firebase";
+import { firebaseApp } from "./app/utils/firebase";
 
 export default function App() {
 
