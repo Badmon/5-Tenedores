1cc7ee16492def53382f95cdec21df3e8c518a02
Juan Antonio <jantoleonh@gmail.com>
2021-01-06 23:35:25 -0500

    Navegando a la Screen de Registro desde la Screen de Login

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -4,7 +4,7 @@ import { StyleSheet, View, Text } from "react-native";
 export default function Register() {
     return(
         <View>
-            <Text></Text>
+            <Text>Register...</Text>
         </View>
 
     );
